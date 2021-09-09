9041c9eb45417f8c4e89b476880a37eb3f3e2701
pryme-svg <edoc.www@gmail.com>
2021-09-08 18:44:18 -0700

    update table css

--- get-arch-pages.sh
+++ get-arch-pages.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+to_delete="man-pages" # use original from kernel.org not arch
+
 
 usage() {
-    echo "Usage: ./update.sh (destination folder)"
+    echo "Usage: ./get-arch-pages.sh (destination folder)"
+	echo "use arch-linux-pages"
     exit 1
 }
 
@@ -13,6 +16,10 @@ mirror="https://mirror.osbeck.com/archlinux"
 wget -e robots=off -m -np -c "$mirror/core/os/x86_64/"
 cd "$mirror/core/os/x86_64/"
 rm *.sig # TODO: exclude sig files in find
+for i in to_delete; do
+	rm "$i*.pkg.tar.zst"
+done
+
 for i in $(find . -name "*\.pkg\.tar.*"); do
     propername=$(echo "$i" | sed 's/.pkg.tar.*//g')
     
